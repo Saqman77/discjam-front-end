9800a9ff894124d0d068d5e0cd3d9b1e4d68db0f
Saqlain Haider <saqlainhaider77@gmail.com>
2025-07-23 19:44:07 +0500

    early work

--- src/components/main/prescreen/PreScreen.tsx
+++ src/components/main/prescreen/PreScreen.tsx
@@ -1,7 +1,16 @@
-
+import './prescreen.scss'
 const PreScreen = () => {
   return (
-    <div>PreScreen</div>
+    <div className="p-container">
+        <div className="p-wrapper">
+            <div className="left">
+                <div className="top"></div>
+            </div>
+            <div className="right">
+                <div className="top"></div>
+            </div>
+        </div>
+    </div>
   )
 }
 
