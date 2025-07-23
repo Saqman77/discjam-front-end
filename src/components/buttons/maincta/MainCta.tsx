42499e27c0d604749c851067f08ca8b4cf349169
Saqlain Haider <saqlainhaider77@gmail.com>
2025-07-23 20:45:37 +0500

    basic

---
+++ src/components/buttons/maincta/MainCta.tsx
@@ -0,0 +1,10 @@
+import './maincta.scss';
+const MainCta = () => {
+  return (
+    <button className="main-cta">
+      BUY TICKETS
+    </button>
+  )
+}
+
+export default MainCta
--- src/components/main/Home.tsx
+++ src/components/main/Home.tsx
@@ -1,9 +1,13 @@
+import MainCta from "@components/buttons/maincta/MainCta"
 import PreScreen from "./prescreen/PreScreen"
 
 
 const Home = () => {
   return (
     <div className="main-container">
+      <div className="cta-container">
+        <MainCta/>
+      </div>
       <PreScreen/>
     </div>
   )
