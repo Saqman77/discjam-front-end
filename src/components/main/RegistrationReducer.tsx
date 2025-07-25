e999c37468728f4f14a7680a26b5efb60768bdee
Saqlain Haider <saqlainhaider77@gmail.com>
2025-07-25 18:08:49 +0500

    Update RegistrationReducer.tsx

--- src/components/main/RegistrationReducer.tsx
+++ src/components/main/RegistrationReducer.tsx
@@ -43,7 +43,6 @@ export function registrationReducer(
     action: RegistrationAction
 ): RegistrationContext {
     switch (action.type) {
-        case: 
         default:
             return state;
     }
