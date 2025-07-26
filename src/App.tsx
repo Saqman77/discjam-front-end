b3222c4fb65ced064469dd725b0d070723efae00
Saqlain Haider <saqlainhaider77@gmail.com>
2025-07-26 17:42:20 +0500

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
 import BackgroundCanvas from '@components/three/BackgroundCanvas'
 import './App.scss'
 import Home from '@components/main/Home'
-function App() {
+import { RouterProvider, createRouter, Route } from '@tanstack/react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactNode } from 'react'
+import { routeTree } from './routeTree.gen';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      retry: 1,
+    }
+  }
+})
+
+const router = createRouter({
+  routeTree,
+  context: {},
+  defaultPreload: 'intent',
+  scrollRestoration: true,
+  defaultStructuralSharing: true,
+  defaultPreloadStaleTime: 0,
+})
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+function App() {
   return (
-    <>
-    <BackgroundCanvas/>
-    <Home/>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <BackgroundCanvas/>
+      <Home />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   )
 }
 
