6e1ff77197e887a4f326e2ed8f8a8e4047f2e130
Saqlain Haider <saqlainhaider77@gmail.com>
2025-07-26 05:05:59 +0500

    Update Home.tsx

--- src/components/main/Home.tsx
+++ src/components/main/Home.tsx
@@ -81,7 +81,7 @@ const BackArrow = () => {
       aria-label="Go back"
     >
       <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M20 8L12 16L20 24" stroke="#222" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M20 8L12 16L20 24" stroke="#FFD700" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
       </svg>
     </button>
   );
